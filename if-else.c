358e570eecade5775aae2b50830032a6eb241469
NureAlam68 <nurealam151068@gmail.com>
2025-04-07 21:27:24 +0600

    if-else

---
+++ if-else.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int tk;
+    scanf("%d", &tk);
+    if(tk >= 100) {
+        printf("I will eat burger\n");
+    } else {
+        printf("I will eat pizza\n");
+    }
+}
